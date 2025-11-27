35bb31d1b85302e23acc9922cae90fe2d65b68db
farshad-heravi <f.n.heravi@gmail.com>
2025-11-27 06:28:35 +0100

    publish the chosen bottle temporarily as a tf

--- wrs25_pose_estimation_module_ros2/spray_action_server.py
+++ wrs25_pose_estimation_module_ros2/spray_action_server.py
@@ -10,6 +10,8 @@ import os
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster
 
 from wrs25_pose_estimation_module_ros2.spray_model_wrapper import SprayModelWrapper
 from wrs25_pose_estimation_module_ros2.action import SprayVision
@@ -29,6 +31,9 @@ class SprayActionServer(Node):
         self.bridge = CvBridge()
         self.latest_frame = None
         
+        # Initialize TF broadcaster
+        self.tf_broadcaster = TransformBroadcaster(self)
+        
         # Subscribe to camera topic
         self.image_subscription = self.create_subscription(
             Image,
@@ -45,6 +50,12 @@ class SprayActionServer(Node):
         )
         self.spray_wrapper = SprayModelWrapper()
         self.get_logger().info(f'Spray Vision Action Server started, subscribed to {camera_topic}')
+
+
+        # define valid area to search for bottle in the image
+        # valid area is a rectangle in the image, defined by the top-left and bottom-right corners
+        self.valid_area = (100, 100, 1280-200, 720-200)
+        self.get_logger().info(f'Valid area: {self.valid_area[0]}, {self.valid_area[1]}, {self.valid_area[2]}, {self.valid_area[3]}')
     
     def image_callback(self, msg):
         """Callback to store the latest camera frame."""
@@ -107,10 +118,29 @@ class SprayActionServer(Node):
 
         # select the bottle for picking 
         bottle_for_picking = self.select_bottle_for_picking(result)
+        if bottle_for_picking is None:
+            self.get_logger().error('No bottle found in the valid area')
+            result_msg = SprayVision.Result()
+            result_msg.success = False
+            result_msg.message = 'No bottle found in the valid area'
+            result_msg.object_ids = []
+            result_msg.tail_x = []
+            result_msg.tail_y = []
+            result_msg.head_x = []
+            result_msg.head_y = []
+            result_msg.centroid_x = []
+            result_msg.centroid_y = []
+            result_msg.orientations = []
+            result_msg.visualization_path = ''
+            goal_handle.abort()
+            return result_msg
         print(f'Bottle for picking: {bottle_for_picking}')
         
+        # Publish TF for the target bottle
+        self.publish_target_bottle_tf(bottle_for_picking)
+        
         # illustrate the bottle for picking
-        self.spray_wrapper.illustrate_result(self.latest_frame, result['objects'], select_bottle=bottle_for_picking)
+        self.spray_wrapper.illustrate_result(self.latest_frame, result['objects'], select_bottle=bottle_for_picking, valid_area=self.valid_area)
 
 
         # Create result message
@@ -161,9 +191,50 @@ class SprayActionServer(Node):
     def select_bottle_for_picking(self, result):
         """Select the bottle for picking."""
         # select the closest bottle to bottom-left corner of the image
-        bottom_left_corner = (0, self.latest_frame.shape[0])
-        closest_bottle = min(result['objects'], key=lambda x: np.abs(np.sqrt((x['centroid'][0] - bottom_left_corner[0])**2 + (x['centroid'][1] - bottom_left_corner[1])**2)))
+        isFound = False
+        closest_bottle = None
+        while not isFound and result['objects']:
+            bottom_left_corner = (0, self.latest_frame.shape[0])
+            closest_bottle = min(result['objects'], key=lambda x: np.abs(np.sqrt((x['centroid'][0] - bottom_left_corner[0])**2 + (x['centroid'][1] - bottom_left_corner[1])**2)))
+
+            # is inside the valid area?
+            if self.valid_area[0] <= closest_bottle['centroid'][0] <= self.valid_area[2] and self.valid_area[1] <= closest_bottle['centroid'][1] <= self.valid_area[3]:
+                isFound = True
+            else:
+                result['objects'].remove(closest_bottle)
+            if not result['objects']:
+                closest_bottle = None
+                break
+
         return closest_bottle
+    
+    def publish_target_bottle_tf(self, bottle, parent_frame='camera_color_optical_frame'):
+        """Publish TF for the target bottle in pixel coordinates."""
+        t = TransformStamped()
+        
+        # Header
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = parent_frame
+        t.child_frame_id = 'target_bottle'
+        
+        # Translation (using pixel coordinates, normalized by image dimensions)
+        # For proper 3D positioning, this should be combined with depth information
+        # Here we just publish the pixel location in the camera optical frame
+        t.transform.translation.x = float(bottle['centroid'][0])  # pixel x
+        t.transform.translation.y = float(bottle['centroid'][1])  # pixel y
+        t.transform.translation.z = 0.0  # depth would go here if available
+        
+        # Rotation based on bottle orientation
+        # Convert orientation angle to quaternion (rotation around z-axis)
+        angle_rad = np.deg2rad(bottle['orientation_deg'])
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = np.sin(angle_rad / 2.0)
+        t.transform.rotation.w = np.cos(angle_rad / 2.0)
+        
+        # Broadcast the transform
+        self.tf_broadcaster.sendTransform(t)
+        self.get_logger().info(f'Published target_bottle TF at pixel coords ({bottle["centroid"][0]}, {bottle["centroid"][1]}) with orientation {bottle["orientation_deg"]}°')
 
 def main(args=None):
     rclpy.init(args=args)
